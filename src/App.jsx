e471ed92fcc7a2eea71c97cff11e77fa2a89ed44
Theresa Husske <theresa.husske@t-online.de>
2023-11-15 22:08:10 +0100

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import "./index.css";
+import "./components/TodoStyle.css";
+import TodoList from "./components/TodoList";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="todo-container">
+        <div className="todo-header">
+          <h1>Your Todo List</h1>
+        </div>
+        <div className="todo-app">
+          <TodoList />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
